1e54d7503e1376c6a772b02627b53245f526e999
y.kukhol@spd-ukraine.com <Eragodyspace512>
2022-04-28 19:41:21 +0300

    1 commit

--- src/main/java/com/Erag0/USBCrypt/crypto/CryptoDataDto.java
+++ src/main/java/com/Erag0/USBCrypt/crypto/CryptoDataDto.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @Getter
 @Setter
-public class CryptoDataDto {
+public class CryptoDataDto1 {
     private boolean isBackupNeeded;
     private byte[] password;
     private List<File> files;
